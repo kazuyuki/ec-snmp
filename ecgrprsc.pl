e5e062420c96e97280199a04a5eee0008c2ebac3
root <root@cent71.mshome.net>
2015-06-23 12:01:12 +0900

    update

--- ecgrprsc.pl
+++ ecgrprsc.pl
@@ -6,23 +6,29 @@ my $logfile = "/root/project/ec-snmp/log";
 #my $base_oid = ".1.3.6.1.4.1.119.2.3.207.1.2.4";
 #my $base_oid = ".1.3.6.1.4.1.8072.2.255";
 my $base_oid = ".1.3.6.1.4.1.2021.255";
-
 my ($opt, $oid)=@ARGV;
-my ($type, $value, @grpname);
-push @grpname, "";
+my ($type, $value, $tmp);
+my (@grpname, @rscname, @rscstat);
+
+@grpname = ();
+@rscname = ();
+@rscstat = ();
 
 open OUT, ">> $logfile";
-print OUT (localtime . " IN  oid=[". $oid . "]\n");
+print OUT (localtime . " [D] IN  oid=[". $oid . "]\n");
 
 #&test;
 #exit;
 
-if ($oid =~ /$base_oid\.(\d+)$/){
-	my $idx = $1;
+open IN, "/opt/nec/clusterpro/bin/clpstat --local|";
 
-	open IN, "/opt/nec/clusterpro/bin/clpstat --local|";
-	while(<IN>){
-		if(/<group>/){
+if ($oid =~ /$base_oid\.1\.(\d+)\.(\d+)\.(\d+)/){
+	# index for group, resource, detail(name & status)
+	my $gidx = $1;
+	my $ridx = $2;
+	my $didx = $3;
+	while (<IN>){
+		if (/<group>/){
 			last;
 		}
 	}
@@ -34,18 +40,37 @@ if ($oid =~ /$base_oid\.(\d+)$/){
 		if (/^\s{4}(\S.*?)\s\.*:/){
 			push @grpname, $1;
 		}
+		if (/^\s{6}(\S.*?)\s.*:\s(.*)$/){
+			if (/^\s{6}current        :/){
+				next;
+			}
+			push @{$rscname[$#grpname]}, $1;
+			push @{$rscstat[$#grpname]}, $2;
+		}
 	}
 
+	if (($gidx - 1 > $#grpname) or ($gidx -1 < 0)){
+		printf OUT (localtime . " [E] group index [%d] is out of range for N of groups [%d]\n", $gidx, $#grpname + 1);
+		exit;
+	}
+	if (($ridx - 1 > $#{$rscname[$#grpname]}) or ($ridx - 1 < 0)){
+		printf OUT (localtime . " [E] resource index [%d] is out of range for N of resources [%d]\n", $ridx, $#{$rscname[$#grpname]} + 1);
+		exit;
+	}
 	$type = "string";
-	$value = $grpname[$idx];
-
-	#print OUT "[D]====\n$oid\n$type\n$value\n[D]====\n";
-	print OUT (localtime . " OUT oid=[". $oid . "] type=[" . $type . "] value=[". $value . "]\n");
-	print "$oid\n$type\n$value\n";
+	if ($didx == 1){
+		$value = $rscname[$gidx-1][$ridx-1];
+		print OUT (localtime . " [D] resource name : $value\n");
+	}
+	if ($didx == 2){
+		$value = $rscstat[$gidx-1][$ridx-1];
+		print OUT (localtime . " [D] resource stat : $value\n");
+	}
 }else{
-	print "[D] else\n";
+	print OUT "[D] else\n";
 }
-
+print OUT (localtime . " [D] OUT oid=[". $oid . "] type=[" . $type . "] value=[". $value . "]\n");
+print "$oid\n$type\n$value\n";
 exit;
 
 sub test { 
