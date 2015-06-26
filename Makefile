38106f8af135a30f4c0839a6334dcfd45cb1824c
root <root@cent71.mshome.net>
2015-06-26 12:32:57 +0900

    2nd release

--- Makefile
+++ Makefile
@@ -1,8 +1,25 @@
+test-walk:
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.1
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.2
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.3
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.1.1
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.1.2
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.1.3
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.1.1.1
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.1.1.2
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.1.1.3
+	snmpwalk -v 2c localhost -c public .1.3.6.1.4.1.2021.255.3.3.3
+
+
+test-next:
+	./ecgrprsc.pl -n .1.3.6.1.4.1.2021.255.1.1.1.2
+	#./ecgrprsc.pl -n .1.3.6.1.4.1.2021.255.1
+
+test-mon:
+	./ecgrprsc.pl -n .1.3.6.1.4.1.2021.255.2
+
 test:
-	#./ecgrprsc.pl -g .1.3.6.1.4.1.8072.2.255.1
-	#./ecgrprsc.pl -g .1.3.6.1.4.1.8072.2.255.2
-	#./ecgrprsc.pl -g .1.3.6.1.4.1.8072.2.255.3
-	#./ecgrprsc.pl -g .1.3.6.1.4.1.119.2.3.207.1.2.4.1024
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.1.1.1
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.1.1.2
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.1.2.1
@@ -12,6 +29,8 @@ test:
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.2.2.1
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.2.2.2
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.2.2.3
+	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.2.3.1
+	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.2.3.2
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.3.1.1
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.3.1.2
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1.3.1.3
@@ -30,4 +49,10 @@ test:
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.1
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.2
 	./ecgrprsc.pl -g .1.3.6.1.4.1.2021.255.3
+
+test-net-snmp:
+	./ecgrprsc.pl -g .1.3.6.1.4.1.8072.2.255.1
+	./ecgrprsc.pl -g .1.3.6.1.4.1.8072.2.255.2
+	./ecgrprsc.pl -g .1.3.6.1.4.1.8072.2.255.3
+	./ecgrprsc.pl -g .1.3.6.1.4.1.119.2.3.207.1.2.4.1024
 
--- ecgrprsc.pl
+++ ecgrprsc.pl
@@ -1,50 +1,146 @@
 #!/usr/bin/perl 
-use strict;
-#use warnings;
 
-my $logfile = "/root/project/ec-snmp/log";
+#
+# EXPRESSCLUSTER SNMP agent script for snmpd
+#
+# 2015.06.25 miyamoto KAZuyuki
+#
+# OID structure
+# -------------
+#
+#           +--------- group resource table
+#           | +------- index of failover group
+#           | | +----- index of resource
+#           | | | +--- 1:resource name 2:resource status
+#           | | | |
+#
+# $base_oid.1.1.1.1
+#
+#           +--------- monitor resource table
+#           | +------- index of monitor resource
+#           | | +----- 1:monitor name 2:monitor status
+#           | | |
+#
+# $base_oid.2.1.1
+#
+
+use strict;
+use warnings;
+
+my $debug = 0;
+my $logfile = "/var/log/ec-snmp.log";
 my $base_oid = ".1.3.6.1.4.1.2021.255";
 my ($opt, $oid)=@ARGV;
 my ($type, $value, $tmp);
 my @grpname = ();
 my @rscname = ();
 my @rscstat = ();
+my @monname = ();
+my @monstat = ();
 
-open OUT, ">> $logfile";
-print OUT (localtime . " [D] IN  oid=[". $oid . "]\n");
+sub dbgprint {
+	if ($debug == 1) {
+		print OUT (localtime . " " . $_[0]);
+	}
+}
+if ( $debug == 1 ) {
+	open OUT, ">> $logfile";
+}
+&dbgprint ("[D] IN  oid=[$oid] opt=[$opt]\n");
 open IN, "/opt/nec/clusterpro/bin/clpstat --local|";
+
+while (<IN>){
+	if (/<group>/){
+		last;
+	}
+}
+while(<IN>){
+	chomp;
+	if(/<monitor>/){
+		last;
+	}
+	if (/^\s{4}(\S.*?)\s\.*:/){
+		push @grpname, $1;
+	}
+	if (/^\s{6}(\S.*?)\s.*:\s(.*?)\s*$/){
+		if (/^\s{6}current        :/){
+			next;
+		}
+		push @{$rscname[$#grpname]}, $1;
+		push @{$rscstat[$#grpname]}, $2;
+	}
+}
+while(<IN>){
+	chomp;
+	if(/^ =+$/){
+		last;
+	}
+	m/^\s{4}(\S.*?)\s.*: (.*?)\s*$/;
+	push @monname, $1;
+	push @monstat, $2;
+}
+
+#
+# next option (-n)
+#
+if ( $opt eq "-n" ) {
+	if ( $oid =~ /$base_oid$/ ) {
+		$oid = $base_oid . ".1.1.1.1";
+	}
+	elsif ( $oid =~ /$base_oid\.1$/ ) {
+		$oid = $base_oid . ".1.1.1.1";
+	}
+	elsif ( $oid =~ /$base_oid\.1\.(\d+)$/ ) {
+		$oid = $base_oid . ".1.$1.1.1";
+	}
+	elsif ( $oid =~ /$base_oid\.1\.(\d+)\.(\d+)$/ ) {
+		$oid = $base_oid . ".1.$1.$2.1";
+	}
+	elsif ( $oid =~ /$base_oid\.1\.(\d+)\.(\d+)\.(\d+)$/ ) {
+		if ( $3 < 2 ) {
+			$oid = $base_oid . ".1.$1.$2." . ($3+1);
+		}
+		elsif ( $2 < $#rscname + 1 ) {
+			$oid = $base_oid . ".1.$1." . ($2+1) . ".1";
+		}
+		elsif ( $1 < $#grpname + 1 ) {
+			$oid = $base_oid . ".1." . ($1+1) . ".1.1";
+		}
+		else {
+			$oid = $base_oid . ".2.1.1";
+		}
+	}
+	elsif ( $oid =~ /$base_oid\.2$/ ) {
+		$oid = $base_oid . ".2.1.1";
+	}
+	elsif ( $oid =~ /$base_oid\.2\.(\d+)$/ ) {
+		$oid = $base_oid . ".2.$1.1";
+	}
+	elsif ( $oid =~ /$base_oid\.2\.(\d+)\.(\d+)$/ ) {
+		if ( $2 < 2 ) {
+			$oid = $base_oid . ".2.$1." . ($2+1);
+		}
+		elsif ( $1 < $#monname + 1 ) {
+			$oid = $base_oid . ".2." . ($1+1) . ".1";
+		}
+		else {
+			&dbgprint ("[D] no next OID\n");
+			exit;
+		}
+	}
+}
+
 if ($oid =~ /$base_oid\.1\.(\d+)\.(\d+)\.(\d+)/){
-	# index for group, resource, detail(name & status)
+	# index for failover-Group, group-Resource, resource-Detail(name & status)
 	my $gidx = $1;
 	my $ridx = $2;
 	my $didx = $3;
-	while (<IN>){
-		if (/<group>/){
-			last;
-		}
-	}
-	while(<IN>){
-		chomp;
-		if(/<monitor>/){
-			last;
-		}
-		if (/^\s{4}(\S.*?)\s\.*:/){
-			push @grpname, $1;
-		}
-		if (/^\s{6}(\S.*?)\s.*:\s(.*?)\s*$/){
-			if (/^\s{6}current        :/){
-				next;
-			}
-			push @{$rscname[$#grpname]}, $1;
-			push @{$rscstat[$#grpname]}, $2;
-		}
-	}
 	if (($gidx - 1 > $#grpname) or ($gidx -1 < 0)){
-		printf OUT (localtime . " [E] group index [%d] is out of range for N of groups [%d]\n", $gidx, $#grpname + 1);
+		&dbgprint ("[E] group index [". $gidx . "] is out of range for N of groups [" . ($#grpname + 1) . "]\n");
 		exit;
 	}
 	if (($ridx - 1 > $#{$rscname[$#grpname]}) or ($ridx - 1 < 0)){
-		printf OUT (localtime . " [E] resource index [%d] is out of range for N of resources [%d]\n", $ridx, $#{$rscname[$#grpname]} + 1);
+		&dbgprint ("[E] resource index [". $ridx . "] is out of range for N of groups [" . ($#{$rscname[$#grpname]} + 1) . "]\n");
 		exit;
 	}
 	$type = "string";
@@ -55,7 +151,8 @@ if ($oid =~ /$base_oid\.1\.(\d+)\.(\d+)\.(\d+)/){
 		$value = $rscstat[$gidx-1][$ridx-1];
 	}
 	else {
-		printf OUT (localtime . " [E] detail index [$didx] is out of range (1..2)\n");
+		dbgprint ("[E] detail index [$didx] is out of range (1..2)\n");
+		exit;
 	}
 }
 elsif ($oid =~ /$base_oid\.2\.(\d+)\.(\d+)/){
@@ -67,19 +164,8 @@ elsif ($oid =~ /$base_oid\.2\.(\d+)\.(\d+)/){
 			last;
 		}
 	}
-	my @monname = ();
-	my @monstat = ();
-	while(<IN>){
-		chomp;
-		if(/^ =+$/){
-			last;
-		}
-		m/^\s{4}(\S.*?)\s.*: (.*?)\s*$/;
-		push @monname, $1;
-		push @monstat, $2;
-	}
 	if(($midx - 1 > $#monname) or ($midx - 1 < 0)){
-		printf OUT (localtime . " [E] monitor index [%d] is out of range for N of monitors [%d]\n", $midx, $#monname + 1);
+		dbgprint ("[E] monitor index [" . $midx . "] is out of range for N of monitors [" . ($#monname + 1) ."]\n");
 		exit;
 	}
 	$type = "string";
@@ -90,9 +176,11 @@ elsif ($oid =~ /$base_oid\.2\.(\d+)\.(\d+)/){
 		$value = $monstat[$midx - 1];
 	}
 }else{
-	print OUT (localtime . " [D] else\n");
+	&dbgprint ("[D] else\n");
 	exit;
 }
-print OUT (localtime . " [D] OUT oid=[". $oid . "] type=[" . $type . "] value=[". $value . "]\n");
+&dbgprint ("[D] OUT oid=[". $oid . "] type=[" . $type . "] value=[". $value . "]\n");
 print "$oid\n$type\n$value\n";
 exit;
+
+
